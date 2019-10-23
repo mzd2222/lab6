e5bb285f2536cb9b153752969b624011e681b53b
mzd_Ship <mzd@mail.ynu.edu.cn>
2019-10-23 22:57:33 +0800

    change HelloController

--- src/main/java/datatest/Controller/HelloController.java
+++ src/main/java/datatest/Controller/HelloController.java
@@ -3,15 +3,14 @@ package datatest.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/")
     public String hello() {
-
-        return "this is a test";
+        return "this is a change test";
     }
-
-
 }
