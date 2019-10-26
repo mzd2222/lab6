a0f9216a72b867bc7c2d0345738c058285e5b479
mzd_Ship <mzd@mail.ynu.edu.cn>
2019-10-26 17:43:52 +0800

    基本完成登录注册功能，剩下与play页面结合。

--- src/main/java/datatest/Controller/HelloController.java
+++ src/main/java/datatest/Controller/HelloController.java
@@ -4,8 +4,14 @@ import datatest.Dao.UserDao;
 import datatest.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -13,16 +19,44 @@ public class HelloController {
     @Autowired
     UserDao userDao;
 
-    @GetMapping("/")
-    public String Hello() {
+    User user;
+
+    @GetMapping("/play")
+    public String Hello(Model model) {
+        model.addAttribute("msg", "monk");
         return "play";
     }
 
     @ResponseBody
-    @GetMapping("/user")
-    public User insertUser(User user) {
+    @PostMapping("/user")
+    public String insertUser(User user) {
+        List<User> all = userDao.findAll();
+        for (User a : all) {
+            if (a.getName().equals(user.getName())) {
+                this.user = a;
+                return "error";
+            }
+        }
         userDao.save(user);
-        return user;
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/log")
+    public Map log(String username) {
+        List<User> all = userDao.findAll();
+        Map<String, String> map = new HashMap<>();
+        for (User a : all) {
+            if (a.getName().equals(username)) {
+                this.user = a;
+                /* TO DO */
+                map.put("status", "ok");
+                map.put("username", this.user.getName());
+                return map;
+            }
+        }
+        map.put("status", "error");
+        return map;
     }
 
 }
